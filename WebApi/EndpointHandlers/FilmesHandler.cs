0ae25f3d17af386cdb7a98014dc2a955a238c23d
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-04-15 21:38:17 -0300

    refactor: the enpoints handler now are in a separated folder

---
+++ WebApi/EndpointHandlers/FilmesHandler.cs
@@ -0,0 +1,56 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using WebApi.DbContexts;
+using WebApi.Entities;
+using WebApi.Models;
+
+namespace WebApi.EndpointHandlers;
+
+public static class FilmesHandler
+{
+    public static IResult GetFilmes(Context context)
+    {
+        return Results.Ok(context.Filmes
+            .Include(filme => filme.Diretor)
+            .OrderByDescending(filme => filme.Ano)
+            .ThenByDescending(filme => filme.Titulo)
+            .ToList());
+    }
+
+    public static IResult GetFilmeById(Context context, int id)
+    {
+        return Results.Ok(context.Filmes
+            .Where(filme => filme.Id == id)
+            .Include(filme => filme.Diretor)
+            .ToList());
+    }
+
+    public static IResult GetFilmeByName(Context context, string titulo)
+    {
+
+        return Results.Ok(context.Filmes
+            .Where(filme => EF.Functions.Like(filme.Titulo, $"%{titulo}%"))
+            //.Where(filme => filme.Titulo.ToLower().Contains(titulo.ToLower()))
+            .Include(filme => filme.Diretor)
+            .ToList());
+    }
+
+    public static IResult UpdateFilme(Context context, FilmeUpdate filmeUpdate)
+    {
+        int affectedRows = context.Filmes
+            .Where(filme => filme.Id == filmeUpdate.Id)
+            .ExecuteUpdate(setter => setter
+                .SetProperty(f => f.Titulo, filmeUpdate.Titulo)
+                .SetProperty(f => f.Ano, filmeUpdate.Ano)
+            );
+        if (affectedRows == 0) return Results.NotFound();
+        return Results.Ok($"Você teve um total de {affectedRows} linha(s) afetada(s)");
+    }
+
+    public static IResult DeleteFilme(Context context, int filmeId)
+    {
+        return Results.Ok(context.Filmes
+            .Where(filme => filme.Id == filmeId)
+            .ExecuteDelete<Filme>());
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,6 +2,7 @@ using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 using WebApi.DbContexts;
+using WebApi.EndpointHandlers;
 using WebApi.Entities;
 using WebApi.Models;
 
@@ -36,146 +37,26 @@ if (app.Environment.IsDevelopment())
 //     context.Database.EnsureCreated();
 // });
 
-app.MapGet("/diretores", (Context context) =>
-{
-    return context.Diretores
-        .Include(diretor => diretor.Filmes)
-        .ToList();
-})
-.WithOpenApi();
+app.MapGet("/diretores", DiretoresHandler.GetDiretores).WithOpenApi();
 
-app.MapGet("/diretores/{id}", (Context context, int id) =>
-{
-    return context.Diretores
-        .Include(diretor => diretor.Filmes)
-        .FirstOrDefault(diretor => diretor.Id == id);
-})
-.WithOpenApi();
+app.MapGet("/diretores/byName/{name}", DiretoresHandler.GetDiretorByName).WithOpenApi();
 
-app.MapGet("/diretores/byName/{name}", (Context context, string name) =>
-{
-    return context.Diretores
-        .Include(diretor => diretor.Filmes)
-        .FirstOrDefault(diretor => diretor.Name.Contains(name))
-        ?? new Diretor { Id = -1, Name = "Inexistente" };
-})
-.WithOpenApi();
+app.MapGet("/diretores/{id}", DiretoresHandler.GetDiretorById).WithOpenApi();
 
-app.MapPost("/diretores", (Context context, Diretor diretor) =>
-{
-    context.Diretores.Add(diretor);
-    context.SaveChanges();
-})
-.WithOpenApi();
+app.MapPost("/diretores", DiretoresHandler.AddDiretor).WithOpenApi();
 
-app.MapPut("/diretores/{diretorId}", (Context context, int diretorId, Diretor diretorNovo) =>
-{
-    var diretor = context.Diretores.Find(diretorId);
+app.MapPut("/diretores/{diretorId}", DiretoresHandler.UpdateDiretor).WithOpenApi();
 
-    if (diretor != null)
-    {
-        diretor.Name = diretorNovo.Name;
-        if (diretorNovo.Filmes.Count > 0)
-        {
-            diretor.Filmes.Clear();
-            foreach (var filme in diretorNovo.Filmes)
-            {
-                diretor.Filmes.Add(filme);
-            }
-        }
-    }
-    context.SaveChanges();
-})
-.WithOpenApi();
+app.MapDelete("/diretores/{diretorId}", DiretoresHandler.DeleteDiretor).WithOpenApi();
 
-app.MapDelete("/diretores/{diretorId}", (Context context, int diretorId) =>
-{
-    var diretor = context.Diretores.Find(diretorId);
+app.MapGet("/filmes", FilmesHandler.GetFilmes).WithOpenApi();
 
-    if (diretor != null)
-    {
-        context.Remove(diretor);
-    }
-    context.SaveChanges();
-})
-.WithOpenApi();
+app.MapGet("/filmes/{id}", FilmesHandler.GetFilmeById).WithOpenApi();
 
-app.MapGet("/filmes", (Context context) =>
-{
-    return context.Filmes
-        .Include(filme => filme.Diretor)
-        .OrderByDescending(filme => filme.Ano)
-        .ThenByDescending(filme => filme.Titulo)
-        .ToList();
-})
-.WithOpenApi();
+app.MapGet("/filmes/byName/{titulo}", FilmesHandler.GetFilmeByName).WithOpenApi();
 
-app.MapGet("/filmes/{id}", (Context context, int id) =>
-{
-    return context.Filmes
-        .Where(filme => filme.Id == id)
-        .Include(filme => filme.Diretor)
-        .ToList();
-})
-.WithOpenApi();
+app.MapPatch("/filmes", FilmesHandler.UpdateFilme).WithOpenApi();
 
-app.MapGet("/filmes/byName/{titulo}", (
-    Context context,
-    string titulo) =>
-{
-    // return context.Filmes
-    //     .Where(filme => filme.Titulo.Contains(titulo))
-    //     .Include(filme => filme.Diretor)
-    //     .ToList();
-
-    return context.Filmes
-        .Where(filme =>
-                    EF.Functions.Like(filme.Titulo, $"%{titulo}%")
-        )
-        .Include(filme => filme.Diretor)
-        .ToList();
-})
-.WithOpenApi();
-
-app.MapPatch("/filmesSemExecuteUpdate", (Context context, FilmeUpdate filmeUpdate) =>
-{
-    var filme = context.Filmes.Find(filmeUpdate.Id);
-
-    if (filme == null) return Results.NotFound($"Filme com id {filmeUpdate.Id} não encontrado");
-
-    filme.Titulo = filmeUpdate.Titulo;
-    filme.Ano = filmeUpdate.Ano;
-
-    context.Filmes.Update(filme);
-    context.SaveChanges();
-    return Results.Ok($"Filme com id {filmeUpdate.Id} atualizado com sucesso");
-});
-
-app.MapPatch("/filmes", (Context context, FilmeUpdate filmeUpdate) =>
-{
-    int affectedRows = context.Filmes
-        .Where(filme => filme.Id == filmeUpdate.Id)
-        .ExecuteUpdate(setter => setter
-            .SetProperty(f => f.Titulo, filmeUpdate.Titulo)
-            .SetProperty(f => f.Ano, filmeUpdate.Ano)
-        );
-
-    if (affectedRows > 0)
-    {
-        return Results.Ok(
-            $"Você teve um total de {affectedRows} linha(s) afetada(s)"
-        );
-    } else {
-        return Results.NoContent();
-    }
-});
-
-app.MapDelete("/filmes/{filmeId}", (Context context, int filmeId) =>
-{
-    var filme = context.Filmes
-        .Where(filme => filme.Id == filmeId)
-        .ExecuteDelete<Filme>();
-    context.SaveChanges();
-});
+app.MapDelete("/filmes/{filmeId}", FilmesHandler.DeleteFilme).WithOpenApi();
 
 app.Run();
